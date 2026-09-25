2f244b1a51fc39448d2893d38fe038a8e5f65aca
Bazyli <franklinmeendres734@gmail.com>
2026-09-25 19:11:17 -0400

    denifion rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TareaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tareas.index');
 });
+
+Route::resources('tareas', TareaController::class);
