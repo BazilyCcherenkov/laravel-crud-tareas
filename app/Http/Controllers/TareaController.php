28a25b86918e29e480b1a4649bd277bc09ebaef3
Bazyli <franklinmeendres734@gmail.com>
2026-09-25 19:28:40 -0400

    implementar logica CRUD en el controlador tarea

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -10,41 +10,57 @@ class TareaController extends Controller
     /**
      * Display a listing of the resource.
      */
+    // listar todas las tareas
+
     public function index()
     {
-        //
+        $tareas = Tarea:latest()->get();
+        return view('tareas.index',compact('tareas'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
+    // formulacion de creacion
     public function create()
     {
-        //
+        return view('tareas.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
+
+    // guardar un nueva tarea
     public function store(Request $request)
     {
-        //
+        $datosValidados = $request->validate([
+            'titulo'    => 'required|string|max:255',
+            'descripcion' => 'nullable|string',
+        ]);
+
+        Tarea::create($datosValidados);
+        return redirect()
+            -> route('tareas.index')
+            ->witch('mensaje','Tarea creada correctamente.');
     }
 
     /**
      * Display the specified resource.
      */
+    //mostrar otro formulario
     public function show(Tarea $tarea)
     {
-        //
+        return view('tareas.show',compact('tarea'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
+    //
     public function edit(Tarea $tarea)
     {
-        //
+        return view('tareas.edit',compact('tarea'));
     }
 
     /**
@@ -52,14 +68,30 @@ class TareaController extends Controller
      */
     public function update(Request $request, Tarea $tarea)
     {
-        //
+        $datosValidados = $request->validate([
+            'titulo'    => 'required|string|max:255',
+            'descripcion' => 'nullable|string',
+            'completada' => 'nullable|boolean',
+
+        ]);
+        $datosValidados['completada']=$request->has('completada');
+        $tarea->update($datosValidados);
+
+        return redirect()
+            ->route('tareas.index')
+            ->with('mensaje','Tarea actualizada correctamente.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
+    // Eliminacion
     public function destroy(Tarea $tarea)
     {
-        //
+        $tarea->delete();
+
+        return redirect()
+            ->route('tareas.index')
+            ->with('mensaje','Tarea eleiminada correctamente.');
     }
 }
