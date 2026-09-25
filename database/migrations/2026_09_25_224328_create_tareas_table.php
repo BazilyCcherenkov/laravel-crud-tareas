1005c0391ea1269494df66e1a1b15bb5bc4c80c4
Bazyli <franklinmeendres734@gmail.com>
2026-09-25 19:03:34 -0400

    Agregamos modelo,migracion y controlador base de Tarea

--- database/migrations/2026_09_25_224328_create_tareas_table.php
+++ database/migrations/2026_09_25_224328_create_tareas_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('tareas', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+            $table->boolean('completada')->default(false);
             $table->timestamps();
         });
     }
